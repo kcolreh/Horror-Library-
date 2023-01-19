eda1ffba6d381c717d395c02a891c9537f4b15f3
Mindaugas <mindaugasgrybauskas@protonmail.com>
2023-01-19 14:17:39 +0100

    Create a constructor for movie info , create an empty array and a function that pushed new movies into the array

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('working');
+const myMovieLibrary = [];
+
+function Movie(title, year, imdbRating, director, status) {
+    this.title = title;
+    this.director = director;
+    this.status = status;
+    this.year = year;
+    this.imdbRating = imdbRating;
+    this.returnMovieInfo = function returnMovieInfo() {
+        return `${title} is made in ${year} and has a rating on IMDB of ${imdbRating}, directed by ${director} and i have ${status} this movie`;
+    };
+}
+const theShining = new Movie('The Shining', '1980', '8.4', 'Stanley Kubrick ', 'watched');
+// const itFollows = new Movie('It Follows', '2014', '6.8', 'David Robert Mitchell', 'watched');
+
+function addMovieToLibrary() {
+    const newShit = theShining.returnMovieInfo() + myMovieLibrary;
+    return myMovieLibrary.push(newShit);
+}
+
+addMovieToLibrary();
+console.log(myMovieLibrary);
