85b3a6bd435a91a52673f9393147d706d3d872b4
Mindaugas <mindaugasgrybauskas@protonmail.com>
2023-01-21 15:49:24 +0100

    Create a function that fetches the data from the inputForm, combine constructor and library function to store all the data in an array

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
+const addMovieBtn = document.getElementById('addMoviebtn');
+const movieForm = document.getElementById('inputForm');
+// const movieCard = document.getElementById('original-card');
 const myMovieLibrary = [];
+let idCounter = 0;
 
-function Movie(title, year, imdbRating, director, status) {
+function Movie(title, year, rating, director, status) {
     this.title = title;
     this.director = director;
     this.status = status;
     this.year = year;
-    this.imdbRating = imdbRating;
+    this.rating = rating;
     this.returnMovieInfo = function returnMovieInfo() {
-        return `${title} is made in ${year} and has a rating on IMDB of ${imdbRating}, directed by ${director} and i have ${status} this movie`;
+        return `${title} is made in ${year} and has a rating on IMDB of ${rating}, directed by ${director} and i have ${status} this movie`;
     };
 }
-const theShining = new Movie('The Shining', '1980', '8.4', 'Stanley Kubrick ', 'watched');
-// const itFollows = new Movie('It Follows', '2014', '6.8', 'David Robert Mitchell', 'watched');
 
-function addMovieToLibrary() {
-    const newShit = theShining.returnMovieInfo() + myMovieLibrary;
-    return myMovieLibrary.push(newShit);
+function movieData() {
+    const movieTitle = document.getElementById('title');
+    const movieDirector = document.getElementById('director');
+    const movieYear = document.getElementById('year');
+    const movieRating = document.getElementById('rating');
+    const movieStatus = document.getElementById('status');
+    const title = movieTitle.value;
+    const director = movieDirector.value;
+    const year = movieYear.value;
+    const rating = movieRating.value;
+    const status = movieStatus.value;
+    return new Movie(title, year, rating, director, status);
 }
 
-addMovieToLibrary();
-console.log(myMovieLibrary);
+function addMovieToLibrary() {
+    const userInput = movieData();
+    const newMovie = userInput.returnMovieInfo() + myMovieLibrary;
+    return myMovieLibrary.push(newMovie);
+}
 
+console.log(myMovieLibrary);
 // Below are functions to add a new card clone to the grid
-const addMovieBtn = document.getElementById('addMoviebtn');
-let idCounter = 0;
 
 function addtogrid() {
     const cardGrid = document.getElementById('card-grid');
@@ -40,4 +53,9 @@ function newCardId() {
 addMovieBtn.addEventListener('click', () => {
     newCardId();
     addtogrid();
+});
+
+movieForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addMovieToLibrary();
 });
